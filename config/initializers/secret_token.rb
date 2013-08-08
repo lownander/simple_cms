0687aac19e1b1931e51aae0f2b92772f23bc1c2e
Emil <admin@Emils-Macbook-PRO-2.local>
2013-08-09 00:53:11 +0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '728d70e14ae59f851ddb11536856d1beec3259b6b1c3ca8f6f42f4135ba5f6b1941cf6556a1c6b4f8daf4d3b08963d8789bf9bf44ed082cf53034cd9269786af'
